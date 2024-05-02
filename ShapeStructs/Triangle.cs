8ac0a39efe326985c5a51034d97de99b6b524bfd
Cyro <BlueCyro@protonmail.com>
2024-05-02 16:21:36 -0500

    Goodies for triangle struct, add wide variant

--- ShapeStructs/Triangle.cs
+++ ShapeStructs/Triangle.cs
@@ -1,15 +1,23 @@
 using System.Numerics;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.X86;
 using BepuUtilities;
 
 
 namespace Paprika;
 
 
-public struct Triangle
+[StructLayout(LayoutKind.Explicit)]
+public struct Triangle // Field offsets help significantly in aligning the memory for this struct
 {
+    [FieldOffset(0)]
     public Vector3 A;
+
+    [FieldOffset(16)]
     public Vector3 B;
+
+    [FieldOffset(32)]
     public Vector3 C;
 
 
@@ -45,7 +53,9 @@ public struct Triangle
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public void ZDivide(out Vector3 oldZ)
     {
-        oldZ = new(A.Z, B.Z, C.Z);
+        oldZ.X = A.Z;
+        oldZ.Y = B.Z;
+        oldZ.Z = C.Z;
 
 
         A /= MathF.Max(A.Z, 0f);
@@ -54,13 +64,19 @@ public struct Triangle
     }
 
 
+    public override readonly string ToString()
+    {
+        return $"<[[{A}]], [[{B}]], [[{C}]]>";
+    }
 
+
+    
     public readonly Vector3 Normal => Vector3.Normalize(Vector3.Cross(B - A, C - A));
     public readonly Vector3 Center => (A + B + C) / 3f;
 }
 
 
-
+[StructLayout(LayoutKind.Sequential, Pack = 32)]
 public struct TriangleWide
 {
     public Vector3Wide A;
@@ -68,52 +84,152 @@ public struct TriangleWide
     public Vector3Wide C;
 
 
-
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public readonly void GetBounds(out Vector3Wide min, out Vector3Wide max)
+    public static void GetBounds(in TriangleWide bundle, out Vector3Wide min, out Vector3Wide max)
     {
-        Vector3Wide.Min(A, B, out min);
-        Vector3Wide.Min(C, min, out min);
+        // Using min like this rather than using two out parameters yields a shorter method from the JIT
+        Vector3Wide.Min(Vector3Wide.Min(bundle.A, bundle.B), bundle.C, out min);
 
-        Vector3Wide.Max(A, B, out max);
-        Vector3Wide.Max(C, max, out max);
+        Vector3Wide.Max(Vector3Wide.Max(bundle.A, bundle.B), bundle.C, out max);
     }
 
 
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public void WidenFrom(in Triangle triangle)
+    public static void Get2DBounds(in TriangleWide bundle, out Vector4Wide bounds)
     {
-        Vector3Wide.Broadcast(triangle.A, out Vector3Wide a);
-        Vector3Wide.Broadcast(triangle.B, out Vector3Wide b);
-        Vector3Wide.Broadcast(triangle.C, out Vector3Wide c);
+        Unsafe.SkipInit(out bounds);
+        ref Vector2Wide boundsMin = ref bounds.AsVector2();
+        ref Vector2Wide boundsMax = ref Unsafe.Add(ref bounds.AsVector2(), 1);
 
-        A = a;
-        B = b;
-        C = c;
+        // Using min like this rather than using two out parameters yields a shorter method from the JIT
+        VectorHelpers.Min(VectorHelpers.Min(bundle.A.AsVector2(), bundle.B.AsVector2()), bundle.C.AsVector2(), out boundsMin);
+
+        VectorHelpers.Max(VectorHelpers.Max(bundle.A.AsVector2(), bundle.B.AsVector2()), bundle.C.AsVector2(), out boundsMax);
     }
 
 
 
-    public readonly Vector3Wide Normal
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void GetTotalBounds(in TriangleWide bundle, out Vector3 min, out Vector3 max)
     {
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        get
-        {
-            Vector3Wide.Cross(B - A, C - A, out Vector3Wide crossed);
-            Vector3Wide.Normalize(crossed, out Vector3Wide normalized);
-            return normalized;
-        }
+        GetBounds(bundle, out Vector3Wide minWide, out Vector3Wide maxWide);
+
+        minWide.X.Min(out min.X);
+        minWide.Y.Min(out min.Y);
+        minWide.Z.Min(out min.Z);
+
+
+        maxWide.X.Max(out max.X);
+        maxWide.Y.Max(out max.Y);
+        maxWide.Z.Max(out max.Z);
+        
     }
 
 
-    public readonly Vector3Wide Center
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void ZDivide(in TriangleWide bundle, out Vector3Wide oldZ, out TriangleWide divided)
     {
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        get
-        {
-            Vector3Wide.Scale(A + B + C, new(1f / 3f), out Vector3Wide scaled);
-            return scaled;
-        }
+        oldZ.X = bundle.A.Z;
+        oldZ.Y = bundle.B.Z;
+        oldZ.Z = bundle.C.Z;
+
+        // The scale function here appears to yield a shorter method than the direct divisions from the JIT
+        Vector3Wide.Scale(bundle.A, MathHelper.FastReciprocal(Vector.Max(bundle.A.Z, Vector<float>.Zero)), out divided.A);
+        Vector3Wide.Scale(bundle.B, MathHelper.FastReciprocal(Vector.Max(bundle.B.Z, Vector<float>.Zero)), out divided.B);
+        Vector3Wide.Scale(bundle.C, MathHelper.FastReciprocal(Vector.Max(bundle.C.Z, Vector<float>.Zero)), out divided.C);
+
+        // divided.A = bundle.A / Vector.Max(bundle.A.Z, Vector<float>.Zero);
+        // divided.B = bundle.B / Vector.Max(bundle.B.Z, Vector<float>.Zero);
+        // divided.C = bundle.C / Vector.Max(bundle.C.Z, Vector<float>.Zero);
+    }
+
+
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void Transform(in TriangleWide bundle, in Matrix4x4Wide transform, out TriangleWide transformed)
+    {
+        Matrix4x4Wide.Transform(bundle.A, transform, out transformed.A);
+        Matrix4x4Wide.Transform(bundle.B, transform, out transformed.B);
+        Matrix4x4Wide.Transform(bundle.C, transform, out transformed.C);
+    }
+
+
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void Broadcast(in Triangle triangle, out TriangleWide bundle)
+    {
+        Vector3Wide.Broadcast(triangle.A, out bundle.A);
+        Vector3Wide.Broadcast(triangle.B, out bundle.B);
+        Vector3Wide.Broadcast(triangle.C, out bundle.C);
+    }
+
+
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void ReadSlot(ref TriangleWide bundle, in int index, out Triangle narrow)
+    {
+        ReadFirst(GatherScatter.GetOffsetInstance(ref bundle, index), out narrow);
+    }
+
+
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void WriteSlot(in Triangle narrow, in int index, ref TriangleWide wide)
+    {
+        Vector3Wide.WriteSlot(narrow.A, index, ref wide.A);
+        Vector3Wide.WriteSlot(narrow.B, index, ref wide.B);
+        Vector3Wide.WriteSlot(narrow.C, index, ref wide.C);
+    }
+
+
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void ReadFirst(in TriangleWide bundle, out Triangle narrow)
+    {
+        // The calls to Vector3Wide.ReadFirst appear to not be inlined, and I currently can't figure out why
+
+        narrow.A.X = bundle.A.X[0];
+        narrow.A.Y = bundle.A.Y[0];
+        narrow.A.Z = bundle.A.Z[0];
+
+        narrow.B.X = bundle.B.X[0];
+        narrow.B.Y = bundle.B.Y[0];
+        narrow.B.Z = bundle.B.Z[0];
+
+        narrow.C.X = bundle.C.X[0];
+        narrow.C.Y = bundle.C.Y[0];
+        narrow.C.Z = bundle.C.Z[0];
+        // Vector3Wide.ReadFirst(bundle.A, out narrow.A);
+        // Vector3Wide.ReadFirst(bundle.B, out narrow.B);
+        // Vector3Wide.ReadFirst(bundle.C, out narrow.C);
+    }
+
+
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void GetCenter(in TriangleWide bundle, out Vector3Wide center)
+    {
+        Vector3Wide.Scale(bundle.A + bundle.B + bundle.C, VectorHelpers.OneThird, out center);
+    }
+
+
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void GetNormal(in TriangleWide bundle, out Vector3Wide normal)
+    {
+        Vector3Wide.Cross(bundle.B - bundle.A, bundle.C - bundle.A, out normal);
+
+        // Using FastReciprocalSquareRoot yields a very slightly shorter method and utilizes the proper
+        // instruction set where applicable
+        Vector3Wide.Scale(normal, MathHelper.FastReciprocalSquareRoot(normal.LengthSquared()), out normal);
+    }
+
+
+    
+    public override readonly string ToString()
+    {
+        return $"<[[{A.X}]], [[{A.Y}]], [[{A.Z}]]>";
     }
 }

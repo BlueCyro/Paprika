4bf0ca0dbfb7308097fb519bce9560c480b45f90
Cyro <BlueCyro@protonmail.com>
2024-05-02 16:24:44 -0500

    Do wide triangle uploads

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ using SharpGLTF.Schema2;
 using SharpGLTF.Geometry;
 using SixLabors.ImageSharp;
 using Image = SixLabors.ImageSharp.Image;
+using BepuUtilities;
+using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
 
 
 namespace Paprika;
@@ -19,6 +23,7 @@ public class Program
     public static float TimeFloat => (float)Time;
     public static double avg;
     public static Triangle[] Uploaded = [];
+    public static DumbBuffer<TriangleWide> WideUploaded;
     public static Stopwatch Timer = new();
     public static readonly int defaultCol = new QuickColor(255, 0, 0, 255).RGBA;
     public static readonly int firstCol = new QuickColor(0, 255, 0, 255).RGBA;
@@ -36,6 +41,20 @@ public class Program
     {
         startTimer.Start();
         Console.WriteLine($"Using vector size of {Vector<byte>.Count * 8} bits for rasterization");
+
+
+        if (Avx.IsSupported)
+            Console.WriteLine("Avx supported");
+        
+        if (Sse2.IsSupported)
+            Console.WriteLine("Sse2 supported");
+
+        if (Ssse3.IsSupported)
+            Console.WriteLine("Ssse3 supported");
+
+        if (Avx512F.IsSupported)
+            Console.WriteLine("Avx512F supported");
+
         var pusher = new PixelPusher("Paprika Renderer", 1280, 1024);
 
         if (args.Length == 0 || !Directory.Exists(Path.GetDirectoryName(args[0])))
@@ -51,20 +70,6 @@ public class Program
             var meshTris = m.EvaluateTriangles().Select<(IVertexBuilder A, IVertexBuilder B, IVertexBuilder C, Material mat), Triangle>(triangle => {
                 Matrix4x4 worldMat = model.LogicalNodes[m.LogicalIndex].WorldMatrix;
                 worldMat.Translation += new Vector3(0f, 0f, 0f);
-                // List<Vector4> transformed = [];
-
-                IVertexBuilder[] points = [triangle.A, triangle.B, triangle.C];
-                Vertex[] vertices = new Vertex[3];
-
-
-                // for (int i = 0; i < points.Length; i++)
-                // {
-                //     var data = points[i].GetGeometry();
-                //     // var normal = data.TryGetNormal(out Vector3 aNorm) ? aNorm : new(0.5f, 0.5f, 1f);
-                //     // var uv = points[i].GetMaterial().GetTexCoord(0);
-
-                //     // vertices[i] = new(Vector3.Transform(data.GetPosition(), worldMat), Vector3.TransformNormal(normal, worldMat), default, uv);
-                // }
 
                 
                 Triangle tri = new(
@@ -85,9 +90,37 @@ public class Program
 
         Uploaded = triangles.ToArray();
 
+        List<TriangleWide> widebatches = [];
+
+
+        for (int i = 0; i < Uploaded.Length; i += Vector<float>.Count)
+        {
+            TriangleWide current = new();
+            for (int j = 0; j < Vector<float>.Count; j++)
+            {
+                int realIndex = Math.Min(i + j, Uploaded.Length - 1);
+                Triangle curTri = Uploaded[realIndex];
+                TriangleWide.WriteSlot(curTri, j, ref current);
+            }
+            widebatches.Add(current);
+        }
+
+        TriangleWide[] wideTris = [.. widebatches];
+        WideUploaded = new(wideTris.Length);
+
+        unsafe
+        {
+            fixed(void* ptr = &wideTris[0])
+            {
+                Unsafe.CopyBlock(WideUploaded, ptr, (uint)(widebatches.Count * Unsafe.SizeOf<TriangleWide>()));
+            }
+
+        }
+
         pusher.StartRender();
     }
 }
+
 
 
 public static class ScreenQuad
